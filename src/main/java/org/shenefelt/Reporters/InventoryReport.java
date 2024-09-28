9fda12f47b86d7750951c7ba9a532c0390e0914f
greg <shenefeltgregory@gmail.com>
2024-09-28 15:15:53 -0700

    Add InventoryReport class
    Introduce a new class, InventoryReport, to display information about a company's inventory, including items, totals, reorder thresholds, cost, and margins. This addition sets up the infrastructure needed for future inventory management features.

---
+++ src/main/java/org/shenefelt/Reporters/InventoryReport.java
@@ -0,0 +1,14 @@
+package org.shenefelt.Reporters;
+
+/**
+ * Display information about a company's inventory - items, total, reorder threshold, cost,
+ * margin.
+ *
+ */
+
+public class InventoryReport
+{
+    public InventoryReport() {}
+
+
+}
